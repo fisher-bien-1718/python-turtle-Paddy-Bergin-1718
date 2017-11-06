1a3a0443708115571a76cabf00d019c1c6f61cea
Kathie-Quick <kathie.quick@bend.k12.or.us>
2017-11-06 07:18:17 -0800

    Update fixme.py

--- 4-errors/fixme.py
+++ 4-errors/fixme.py
@@ -9,7 +9,8 @@ tur = turtle.Turtle()
 tur.speed(0)
 
 speed = 0
+speed = speed + 0.1 #A faster way to write an addition equation like this is: speed += 0.1
 for _ in range(1, 300):
     tur.forward(speed)
     tur.goleft(20)
-speed += 0.1
+
